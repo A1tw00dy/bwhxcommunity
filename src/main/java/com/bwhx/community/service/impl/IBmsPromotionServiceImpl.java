0b2391f2e7bd0121d3024607d08286fe02835833
A1tw00dy <403215130@qq.com>
2021-03-24 23:02:01 +0800

    链接后端完成

---
+++ src/main/java/com/bwhx/community/service/impl/IBmsPromotionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bwhx.community.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.bwhx.community.mapper.BmsPromotionMapper;
+import com.bwhx.community.model.entity.BmsPromotion;
+import com.bwhx.community.service.IBmsPromotionService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class IBmsPromotionServiceImpl extends ServiceImpl<BmsPromotionMapper, BmsPromotion> implements IBmsPromotionService {
+
+}
